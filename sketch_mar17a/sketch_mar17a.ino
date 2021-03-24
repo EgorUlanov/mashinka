2d0e9eb3f0d386cad05d616905ef5abc8693150d
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2021-03-24 12:15:31 +0500

    dalnomer

--- sketch_mar17a/sketch_mar17a.ino
+++ sketch_mar17a/sketch_mar17a.ino
@@ -1,10 +1,14 @@
+// дальномер
+int echoPin = 7; 
+int trigPin = 6;
+//Двигатель A
 int ENA = 3;
 int in1 = 4;
 int in2 = 5;
 // Двигатель B
-int ENB = 11;
+int ENB = 9;
 int in3 = 10;
-int in4 = 9;
+int in4 = 11;
  
 void setup()
 {
@@ -15,37 +19,66 @@ void setup()
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
 }
-void loop()
+void dictance()
+{
+  int duration, cm; 
+    digitalWrite(trigPin, LOW); 
+    delayMicroseconds(2); 
+    digitalWrite(trigPin, HIGH); 
+    delayMicroseconds(10); 
+    digitalWrite(trigPin, LOW); 
+    duration = pulseIn(echoPin, HIGH); 
+    cm = duration / 58;
+    Serial.print(cm); 
+    Serial.println(" cm"); 
+}
+void stop()
+{
+  Serial.println("ostanovilsia");
+  digitalWrite(11,0);
+  digitalWrite(10,0);
+  digitalWrite(4,0);
+  digitalWrite(5,0);
+   analogWrite(ENA,0);
+  analogWrite(ENB,0);
+  delay(2000);
+}
+void vpered ()
 {
   Serial.println("vpered");
+  digitalWrite(11,1);
+  digitalWrite(10,0);
+  digitalWrite(4,1);
+  digitalWrite(5,0);
   analogWrite(ENA,100);
   analogWrite(ENB,100);
   delay(2000);
-  Serial.println("ostanovilsia");
-   analogWrite(ENA,0);
-  analogWrite(ENB,0);
-  delay(2000);
+  stop();
+}
+
+void povorot()
+{
+  Serial.println("povarachivay");
+  digitalWrite(11,1);
+  digitalWrite(10,0);
+  digitalWrite(4,1);
+  digitalWrite(5,0);
    analogWrite(ENA,100);
   analogWrite(ENB,0);
   delay(2000);
-   analogWrite(ENA,0);
-  analogWrite(ENB,0);
-  delay(2000);
-   analogWrite(ENA,100);
-  analogWrite(ENB,100);
-  delay(2000);
-   analogWrite(ENA,0);
-  analogWrite(ENB,0);
-  delay(2000);
-   analogWrite(ENA,100);
-  analogWrite(ENB,0);
-  delay(2000);
-   analogWrite(ENA,0);
-  analogWrite(ENB,0);
-  delay(2000);
+  stop();
+}
+void loop()
+{
+  dictance();
+  vpered();
+  
+  povorot();
+  
+ // vpered();
+ 
+ // povorot();
 }
