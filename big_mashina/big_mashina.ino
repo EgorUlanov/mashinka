0e7e2112eece773a67bc2cf790b4dfcf9263a78c
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2021-04-05 11:46:02 +0500

    available

--- big_mashina/big_mashina.ino
+++ big_mashina/big_mashina.ino
@@ -1,49 +1,65 @@
-/*  PRIZM Controller example program
- *  Spin DC motor channel 1 for 5 seconds, then coast to a stop.
- *  After stopping, wait for 2 seconds, spin in opposite direction.
- *  Continue to repeat until RED reset button is pressed.
- *  author PWU on 08/05/2016
-*/
+//1 мотор - ближе к батарейкам
+//2 мотор - дальше к батарейкам
 
-  #include <PRIZM.h>    // include the PRIZM library in the sketch
-  PRIZM prizm;          // instantiate a PRIZM object “prizm” so we can use its functions
-
+  #include <PRIZM.h>    
+  PRIZM prizm; 
+  int incomingByte = 0;
+  
 void setup() {        
-
-  prizm.PrizmBegin();   // Initialize the PRIZM controller
-}
-void vpered()
-{
-   prizm.setMotorPower(1,25);
-  prizm.setMotorPower(2,-20);
-  delay(5000);
-  ostanovka();
-}
-void povorot_vlevo()
-{
-  prizm.setMotorPower(1,25);
-  prizm.setMotorPower(2,20);
-  delay(3200);
-  ostanovka();
-}
-void povorot_vpravo()
-{
-  prizm.setMotorPower(1,-25);
-  prizm.setMotorPower(2,-20);
-  delay(3200);
-  ostanovka();
+  Serial.begin(9600);
+  prizm.PrizmBegin();   
 }
 void ostanovka()
 {
+  Serial.println("ostanovilsia");
   prizm.setMotorPower(1,0);
   prizm.setMotorPower(2,0);
   delay(2000);
 }
+void vpered()
+{
+  Serial.println("vpered");
+  prizm.setMotorPower(1,25);
+  prizm.setMotorPower(2,-20);
+  delay(500);
+  ostanovka();
+}
+void povorot_vlevo()
+{
+  Serial.println("povarachivay");
+  prizm.setMotorPower(1,25);
+  prizm.setMotorPower(2,19);
+  delay(1700);
+  ostanovka();
+}
+void povorot_vpravo()
+{
+  Serial.println("povarachivay");
+  prizm.setMotorPower(1,-25);
+  prizm.setMotorPower(2,-19);
+  delay(1700);
+  ostanovka();
+}
+
 void loop() {     
-  vpered();
-  povorot_vlevo();
-  vpered();
-  povorot_vlevo();
+  if (Serial.available()) {  //если есть доступные данные
+        // считываем байт
+        incomingByte = Serial.read();
+        if (incomingByte == 49){
+          vpered();
+        }
+        if (incomingByte == 50){
+          povorot_vlevo();
+        }
+        if (incomingByte == 51){
+          povorot_vpravo();
+        }
+        // отсылаем то, что получили
+        Serial.print("I received: ");
+        Serial.println(incomingByte, DEC);
+    }
+
+;
                  
   
 }
